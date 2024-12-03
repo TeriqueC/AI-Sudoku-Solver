72b04d52fccc62067bb780533d08852d753cf071
Terique <zlac286@live.rhul.ac.uk>
2024-12-03 19:28:17 +0000

    developing report

--- product/Main.py
+++ product/Main.py
@@ -1,3 +1,4 @@
+import time
 from Sudoku import print_grid, grid_1, grid_2, grid_3, u_grid
 from Algorithms import backtracking
 
@@ -8,20 +9,24 @@ print(U)
 print_grid(grid_1)
 print(S)
 print_grid(backtracking(grid_1))
+time.sleep(2)
 
 print(U)
 print_grid(grid_2)
 print(S)
 print_grid(backtracking(grid_2))
+time.sleep(2)
 
 print(U)
 print_grid(grid_3)
 print(S)
 print_grid(backtracking(grid_3))
+time.sleep(2)
 
 print(U)
 print_grid(u_grid)
 print(S)
 print_grid(backtracking(u_grid))
+time.sleep(2)
 
 # a simple interface to link the printing and solving functionalities
